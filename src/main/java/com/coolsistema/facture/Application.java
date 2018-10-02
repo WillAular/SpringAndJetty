ba86ac7682c56c3eb83b0153519d5efb0a2e6a76
Wiljac Aular <wiljac.aular@almundo.com>
2018-10-02 16:30:25 -0300

    Spring Boot configurado con Jetty

--- src/main/java/com/coolsistema/facture/Application.java
+++ src/main/java/com/coolsistema/facture/Application.java
@@ -8,10 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class AfipConnectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AfipConnectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 	@Bean
 	public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory() {
